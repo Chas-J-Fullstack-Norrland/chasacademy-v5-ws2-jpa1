303efc7820e35eb234dd10e6463c2dd324b4222b
Johan Romin <johan.romin@greycode.se>
2025-12-08 15:06:02 +0100

    JPA Entitets objekt för en bloggpost.

---
+++ src/main/java/se/chasacademy/databaser/wsjpa/blog/models/Post.java
@@ -0,0 +1,64 @@
+package se.chasacademy.databaser.wsjpa.blog.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int postId;
+    @Column
+    private String title, posttext, author;
+    @Column
+    private LocalDateTime publishDate, createDate;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosttext() {
+        return posttext;
+    }
+
+    public void setPosttext(String posttext) {
+        this.posttext = posttext;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDateTime publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+}
