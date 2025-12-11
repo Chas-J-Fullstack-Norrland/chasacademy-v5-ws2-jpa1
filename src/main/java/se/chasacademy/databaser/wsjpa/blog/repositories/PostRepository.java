c0d45240793a9d65ba4b50facb3e9ca9b236ea63
Svartakatten <rikexhx@gmail.com>
2025-12-11 14:55:49 +0100

    added search functions

--- src/main/java/se/chasacademy/databaser/wsjpa/blog/repositories/PostRepository.java
+++ src/main/java/se/chasacademy/databaser/wsjpa/blog/repositories/PostRepository.java
@@ -1,6 +1,7 @@
 package se.chasacademy.databaser.wsjpa.blog.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import se.chasacademy.databaser.wsjpa.blog.models.Post;
 
 import java.util.List;
@@ -8,5 +9,8 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findAllByOrderByPublishDateDesc();
+
+    @Query("SELECT p FROM Post p WHERE p.postId NOT IN (SELECT c.post.postId FROM Comment c WHERE c.flag = true)")
+    List<Post> findAllSafePosts();
 
 }
