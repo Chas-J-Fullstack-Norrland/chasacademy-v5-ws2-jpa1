4b2f6c96e3b65473607740fc5e017da046c64e49
Joso1207 <joso1207@student.miun.se>
2025-12-11 13:27:18 +0100

    Changed SQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,6 @@
+Drop table if exists comment;
+drop table if exists post;
+
 
 create table post (
     post_id serial primary key,
@@ -10,16 +13,12 @@ create table post (
 
 create table comment (
     comment_id serial primary key,
+    post_id int references post(post_id),
+    flag boolean default false,
     title varchar(64) not null,
     comment text not null,
     comment_date timestamp default now()
 );
 
-
-create table post_comments (
-    comment_id int not null references comment(comment_id),
-    post_id int not null references post(post_id),
-    primary key(comment_id, post_id)
-);
 
 insert into post values(1,'Första inlägget', 'I en enkel bloggdatabas.','Admin', now(), now() - INTERVAL '2 days');
